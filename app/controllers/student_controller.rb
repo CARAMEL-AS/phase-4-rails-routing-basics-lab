f70e1057bcdaeed904fc59691ddab1798970ae0e
Aftab Sidhu <Aftab180@gmail.com>
2022-06-29 20:56:36 -0400

    commit tree 7888dacfc0845116f9a7eaf771c04d84683ca38c

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,11 @@
+class StudentController < ApplicationController
+
+    def index
+        render json: Student.all
+    end
+
+    def grades
+        render json: Student.all.sort_by(&:grade).reverse
+    end
+
+end
